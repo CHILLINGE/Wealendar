0b8a8ebe1002952998ae2a07ff2bdf24873ce77a
TEKITER-PC <tekiter8@gmail.com>
2019-01-08 19:41:09 +0900

    Add ICalendarSchedule interface

---
+++ Wealendar/Wealendar/Schedules/IScheduleManager.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wealendar
+{
+    public interface IScheduleManager
+    {
+        // 데이터들을 저장하는 프로퍼티
+        /// <summary>
+        /// 데이터들을 저장하는 프로퍼티
+        /// </summary>
+        Dictionary<DateTime,string> Data { get; set; }
+
+        
+        /// <summary>
+        /// 전체 데이터를 파일에서 로드
+        /// </summary>
+        void Load();
+
+        
+        /// <summary>
+        /// 전체 데이터를 파일에서 저장
+        /// </summary>
+        void Save();
+
+        
+        /// <summary>
+        /// 원하는 날짜의 데이터 가져오기
+        /// </summary>
+        /// <param name="target">원하는 날짜</param>
+        /// <returns></returns>
+        string GetData(DateTime target);
+
+
+        /// <summary>
+        /// 원하는 날짜에 데이터 넣기
+        /// </summary>
+        /// <param name="target">원하는 날짜</param>
+        /// <param name="data">넣을 데이터</param>
+        void SetData(DateTime target, string data);
+    }
+}
