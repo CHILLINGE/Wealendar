48273ec09c455b8b8593ab525c03c8394cd605aa
TEKITER-PC <tekiter8@gmail.com>
2019-01-09 23:50:12 +0900

    Add comment

--- Wealendar/Wealendar/MainWindow.xaml.cs
+++ Wealendar/Wealendar/MainWindow.xaml.cs
@@ -24,7 +24,12 @@ namespace Wealendar
     {
         WeatherManager weather;
 
+
+
         int _currentMonth;
+        /// <summary>
+        /// 현재 달력에 표시되는 달
+        /// </summary>
         public int CurrentMonth
         {
             get
@@ -34,12 +39,15 @@ namespace Wealendar
             set
             {
                 _currentMonth = value;
-                calendar.Month = _currentMonth;
-                txt_month.Text = _currentMonth.ToString() + "월";
+                calendar.Month = _currentMonth; // 달력의 월을 실제로 변경
+                txt_month.Text = _currentMonth.ToString() + "월"; // 달력 위의 글자 변경
             }
         }
 
         int _currentYear;
+        /// <summary>
+        /// 현재 달력의 표시되는 년도
+        /// </summary>
         public int CurrentYear
         {
             get
@@ -49,8 +57,8 @@ namespace Wealendar
             set
             {
                 _currentYear = value;
-                calendar.Year = _currentYear;
-                txt_year.Text = _currentYear.ToString() + "월";
+                calendar.Year = _currentYear; // 달력의 년을 실제로 변경
+                txt_year.Text = _currentYear.ToString() + "월"; // 달력 위의 글자 변경
             }
         }
         
@@ -67,6 +75,7 @@ namespace Wealendar
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
+            // 프로퍼티 초기값 설정
             CurrentMonth = DateTime.Now.Month;
             CurrentYear = DateTime.Now.Year;
             
@@ -74,12 +83,14 @@ namespace Wealendar
             weather.LoadWeather();
         }
 
+        // 달력의 날짜를 누를 때 이벤트
         private void CalendarControl_SelectionChanged(object sender, CalendarEventArgs e)
         {
             txt_datenow.Text = e.TargetDate.ToLongDateString();
 
         }
 
+        // 월 변경 버튼 클릭
         private void btn_month_up_Click(object sender, RoutedEventArgs e)
         {
             if (CurrentMonth < 12)
@@ -93,7 +104,7 @@ namespace Wealendar
             }
                 
         }
-
+        // 월 변경 버튼 클릭
         private void btn_month_down_Click(object sender, RoutedEventArgs e)
         {
             if (CurrentMonth > 1)
