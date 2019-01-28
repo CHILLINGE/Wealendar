13cd514ca3f9cf806f5453fa08ef5356d05a4315
EZYOON <fullmoon5110@gmail.com>
2019-01-29 00:24:56 +0900

    혹시 필요할지 모르는 헤더

--- Wealendar/Wealendar/Weathers/WeatherManager.cs
+++ Wealendar/Wealendar/Weathers/WeatherManager.cs
@@ -3,6 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Xml;
+using System.Xml.Linq;
 
 namespace Wealendar
 {
