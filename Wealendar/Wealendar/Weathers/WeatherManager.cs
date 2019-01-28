91ea0c91f197a5e3c00af6e1cc8600c32ecffe25
hi2yeoni <hi2yeoni@naver.com>
2019-01-29 02:15:01 +0900

    버튼 누르면 날씨정보 가져오기 (미완성)

--- Wealendar/Wealendar/Weathers/WeatherManager.cs
+++ Wealendar/Wealendar/Weathers/WeatherManager.cs
@@ -24,21 +24,54 @@ namespace Wealendar
         /// 웹에 접근하는 클래스
         /// </summary>
         private readonly WebManager webclient;
-
-
+        private string path;
 
         public WeatherManager()
         {
             webclient = new WebManager();
+            int date = 20190128;
+            int time = 1800;
+            int nx = 59;
+            int ny = 126;
+            Dictionary<string, string> data = new Dictionary<string, string>();
+            data["ServiceKey"] = "yW0fFl3x75%2Fc%2FC1jrkPKbqvt49hJS%2FHnk97M2euq1U3cpz%2FB6PyGwLPndqhOVFspMOXaI%2Fnsv0fQZCTQL2xyXw%3D%3D";
+            data["base_date"]= date.ToString();
+            data["base_time"] = time.ToString();
+            data["nx"] = nx.ToString();
+            data["ny"] = ny.ToString();
+
+            path = webclient.GetContent("http://newsky2.kma.go.kr/service/SecndSrtpdFrcstInfoService2", data);
             
+
         }
-
-
 
         public void LoadWeather()
         {
-            
-                
+            XmlDocument docx = new XmlDocument();
+            Dictionary<string, string> Result = new Dictionary<string, string>();
+            try
+            {
+                docx.Load(path);
+                XmlNodeList xmlNodeList = docx.SelectNodes("weather");
+
+                foreach(XmlNode xml in xmlNodeList)
+                {
+                    string date = xml["base_date"].InnerText.ToString();
+                    string time = xml["base_time"].InnerText.ToString();
+                    string weather = xml["fcstValue"].InnerText.ToString();
+
+                    Result[date] = time;
+                    Result[time] = weather;
+                }
+            }
+
+            catch (ArgumentException ex)
+            {
+                MessageBox.Show("XML 문제 발생\n" + ex);
+            }
+
+
+
         }
 
 
