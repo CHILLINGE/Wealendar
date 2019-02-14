a607594c74fd34da7893c38999ab7015d203b91f
EZYOON <fullmoon5110@gmail.com>
2019-02-14 22:28:46 +0900

    Edit XmlDocument code

--- Wealendar/Wealendar/Weathers/WeatherManager.cs
+++ Wealendar/Wealendar/Weathers/WeatherManager.cs
@@ -12,6 +12,7 @@ using System.Windows.Navigation;
 using System.Xml;
 using System.Xml.Linq;
 using System.Web;
+using System.Diagnostics;
 
 namespace Wealendar
 {
@@ -54,8 +55,20 @@ namespace Wealendar
         {
             string output = getAPIString(time, position);
 
+            string tmpoutput = "<?xml version=\"1.0\" encoding=\"UTF - 8\" standalone=\"yes\"?><response><header><resultCode>0000</resultCode><resultMsg>OK</resultMsg></header><body><items><item><regId>11B20201</regId><taMax10>7</taMax10><taMax3>2</taMax3><taMax4>2</taMax4><taMax5>4</taMax5><taMax6>5</taMax6><taMax7>5</taMax7><taMax8>6</taMax8><taMax9>7</taMax9><taMin10>2</taMin10><taMin3>-2</taMin3><taMin4>-4</taMin4><taMin5>-5</taMin5><taMin6>-2</taMin6><taMin7>-1</taMin7><taMin8>0</taMin8><taMin9>1</taMin9></item></items><numOfRows>10</numOfRows><pageNo>1</pageNo><totalCount>1</totalCount></body></response>";
 
             Weather weather = new Weather();
+            
+            XmlDocument xml = new XmlDocument();
+            xml.LoadXml(tmpoutput); // suppose that myXmlString contains "<Names>...</Names>"
+            Console.WriteLine(tmpoutput);
+            XmlNodeList xnList = xml.SelectNodes("/response/header");
+            //foreach (XmlNode xn in xnList)
+            //{
+            //    string firstName = xn["FirstName"].InnerText;
+            //    string lastName = xn["LastName"].InnerText;
+            //    Console.WriteLine("Name: {0} {1}", firstName, lastName);
+            //}
 
             // 여기에 xml 해석해서 Weather 클래스에 채워넣기
 
@@ -64,30 +77,30 @@ namespace Wealendar
 
 
             
-            XmlDocument docx = new XmlDocument();
-            Dictionary<string, string> Result = new Dictionary<string, string>();
-            try
-            {
-                docx.Load(path);
-                XmlNodeList xmlNodeList = docx.SelectNodes("weather");
+            //XmlDocument docx = new XmlDocument();
+            //Dictionary<string, string> Result = new Dictionary<string, string>();
+            //try
+            //{
+            //    docx.Load(path);
+            //    XmlNodeList xmlNodeList = docx.SelectNodes("weather");
 
-                foreach (XmlNode xml in xmlNodeList)
-                {
-                    string Date = xml["baseDate"].InnerText.ToString();
-                    string Time = xml["baseTime"].InnerText.ToString();
-                    string Weather = xml["fcstValue"].InnerText.ToString();
+            //    foreach (XmlNode xml in xmlNodeList)
+            //    {
+            //        string Date = xml["baseDate"].InnerText.ToString();
+            //        string Time = xml["baseTime"].InnerText.ToString();
+            //        string Weather = xml["fcstValue"].InnerText.ToString();
 
-                    /*
-                    Result[date] = time;
-                    Result[time] = weather;
-                    */
-                }
-            }
+            //        /*
+            //        Result[date] = time;
+            //        Result[time] = weather;
+            //        */
+            //    }
+            //}
 
-            catch (ArgumentException ex)
-            {
-                MessageBox.Show("XML 문제 발생\n" + ex);
-            }
+            //catch (ArgumentException ex)
+            //{
+            //    MessageBox.Show("XML 문제 발생\n" + ex);
+            //}
             
 
         }
