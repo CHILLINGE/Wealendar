8014e40c75341d958215ca59ad3ea3ad6421c501
TEKITER-PC <tekiter8@gmail.com>
2019-02-27 00:21:27 +0900

    add team name

--- Wealendar/Wealendar/Properties/AssemblyInfo.cs
+++ Wealendar/Wealendar/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Wealendar")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Copyright © 2019 Team Nullcaffeine")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
